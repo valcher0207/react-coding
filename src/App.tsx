f9dff86c22d0eef7505d7b4b78bf08a693909fff
Valeska Cheryl <valeskacheryl@gmail.com>
2022-03-08 13:04:52 +0700

    another commit but no significant change

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import Component2 from './component/Component2'
 
 
 class App extends React.Component<any> {
-  render() {
-    console.log("render called ...")
-    return (
-      <div style={{textAlign: 'center'}}>
-        <Component1 nama="Valeska" umur={15} kelas="XE" pagi={true}/>
-        <h1>
-          <Component2/>
-        </h1>
-      </div>
-    );
-  }
+    render() {
+        console.log("render called ...")
+        return (
+        <div style={{textAlign: 'center'}}>
+            <Component1 nama="Valeska" umur={15} kelas="XE" pagi={true}/>
+            <h1>
+                <Component2/>
+            </h1>
+        </div>
+        );
+    }
 }
 
 export default App;
